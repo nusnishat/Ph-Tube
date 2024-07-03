ec35d043a0d291c0292d8ad90e5abd4390cda20f
unknown <nusnishat1999@gmail.com>
2024-07-03 21:42:16 +0600

    third commit

--- js/index.js
+++ js/index.js
@@ -3,6 +3,42 @@ const loadData = id =>{
     .then(res => res.json())
     .then(data=> displayVidoes(data.data))
 }
-const displayVidoes = data =>{
-    console.log(data);
+const displayVidoes = vidoes =>{
+    const vidoesContainer = document.getElementById('vidoes-container');
+    vidoesContainer.textContent = '';
+    vidoes.forEach(video=>{
+        const div = document.createElement('div');
+        div.classList.add('card', 'bg-base-100', 'shadow-lg');
+        div.innerHTML = `
+        <figure class="h-56">
+            <img class="h-56"
+            src=${video.thumbnail}
+            />
+        </figure>
+        <div class="p-4 space-y-2">
+            <div class="flex">
+              <img
+               src=${video.authors[0].profile_picture}
+               class="w-8 h-8 me-2 rounded-full"
+               />
+               <h2 class="text-xl font-semibold">
+                ${video.title}
+                </h2>
+            </div>
+            <span class="text-slate-600 ms-10 text-sm">${video.authors[0].profile_name}</span>
+            <span id="verified" class="ms-2 hidden font-semibold text-blue-700">Verified</span>
+            <p class="text-slate-600 ms-10 text-sm">${video.others.views}</p>
+            </div>
+        </div>
+        `
+        const verified = document.getElementById('verified');
+        if(video.authors[0].verified)
+            {
+                verified.classList.remove('hidden')
+            }  
+        vidoesContainer.appendChild(div);
+
+    })
 }
+
+loadData(1000);
